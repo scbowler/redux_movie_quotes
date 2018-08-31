c0d86e3e95efbc5192558d648410522307d814d9
Scott Bowler <scott.bowler@learningfuze.com>
2018-08-31 11:45:10 -0700

    Redux setup complete

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+import types from './types';
+
+export const signIn = () => ({type: types.SIGN_IN});
+
+export const signOut = () => ({type: types.SIGN_OUT});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
 
 import App from './components/app';
 
+const store = createStore(rootReducer, {}, applyMiddleware());
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
